de491c3e2c15b666931f7279f3b90752e4352bec
TashaPo <tanusa1910@mail.ru>
2023-10-18 21:21:55 +0300

    .

--- src/Program.cs
+++ src/Program.cs
@@ -6,68 +6,88 @@ using System.Threading.Tasks;
 
 namespace LifeGame
 {
-    public class Life
-    {
-        int n;
-        int m;
-        int[,] arr;
-        public int N
-        {
-            get { return n; }
-            set { n = value; }
-        }
-        public int M
-        {
-            get { return m; }
-            set { m = value; }
-        }
-        public void SetArr()
-        {
-            arr = new int[n, m];
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < m; j++)
-                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
-        }
-        public void ReadArr()
-        {
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < m; j++)
-                    Console.Write(arr[i, j].ToString() + " ");
-                Console.WriteLine("");
-            }
-        }
-        public void RandomSetArr()
-        {
-            arr = new int[n, m];
-            Random rnd = new Random();
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < m; j++)
-                    arr[i, j] = rnd.Next(0, 2);
-        }
-    }
     class Program
     {
+        //проверка на пустое поле (все клетки мертвы)
+        public static bool AreAllDead(Life l)
+        {
+            int k = 0;
+            for (int i = 0; i < l.N; i++)
+                for (int j = 0; j < l.M; j++)
+                    if (l.GetValue(i, j) == 0) k++;
+            if (k == l.N * l.M) return true;
+            else return false;
+        }
+        //вывод поля
+        public static void ReadArr(Life l)
+        {
+            for (int i = 0; i < l.N; i++)
+            {
+                for (int j = 0; j < l.M; j++)
+                    Console.Write(l.GetValue(i, j).ToString() + " ");
+                Console.WriteLine();
+            }
+        }
+        //заполнение поля в классическом режиме
+        public static void SetArr(ref Life l)
+        {
+            Console.WriteLine("Введите число, которое будет инициализировать клетку <живой> или <мертвой>");
+            Console.WriteLine("Каждое значение с новой строки");
+            Console.WriteLine("1. Клетка <живая>");
+            Console.WriteLine("2. Клетка <мертвая>");
+            //создаем пустой массив поля
+            l.CreateArr();
 
+            for (int i = 0; i < l.N; i++)
+            {
+                for (int j = 0; j < l.M; j++)
+                {
+                    Console.WriteLine("Клетка i = " + i.ToString() + ", j = " + j.ToString());
+                    int a = -1;
+                    do
+                    {
+                        string str1 = Console.ReadLine();
+                        //проверяем, является ли числом вводимые данные
+                        if (int.TryParse(str1, out int number)) a = Convert.ToInt32(str1);
+                    } while (a != 0 && a != 1);
+                    //присваиваем значение ячейки поля
+                    l.SetValue(i, j, a);
+                }
+            }
+        }
+        //меню игры - выбор режима, установка размерности поля
+        public static int Menu(Life l)
+        {
+            Console.WriteLine("Выберите режим игры");
+            Console.WriteLine("1. Автоматическая игра (первоначальная позиция <живых> клеток расставляется рандомно)");
+            Console.WriteLine("2. Классическая игра (пользователь сам расставляет <живые> клетки)");
+
+            int a = 0;
+            do
+            {
+                //вводим номер режима
+                string str1 = Console.ReadLine();
+                //проверяем, является ли числом вводимые данные
+                if (int.TryParse(str1, out int number)) a = Convert.ToInt32(str1);
+            } while (a != 1 && a != 2);
+
+            Console.WriteLine("Введите размерность поля N*M в одну строку");
+            do
+            {
+                //обнуляем предыдущие результаты ввода (если одно из вводимых значений оказалось числом)
+                l.N = 0;
+                l.M = 0;
+                //вводим размерность поля
+                string[] str = Console.ReadLine().Split();
+                //проверяем, является ли числом вводимые данные
+                if (int.TryParse(str[0], out int number)) l.N = int.Parse(str[0]);
+                if (int.TryParse(str[1], out int munber)) l.M = int.Parse(str[1]);
+            } while (l.N == 0 || l.M == 0);
+
+            return a;
+        }
         static void Main(string[] args)
         {
-            //цикл проверок (рекурсия шагов игры)
-            void Step(Life l)
-            {
-
-            }
-            //меню игры - выбор режима, установка размерности поля
-            int Menu(Life l)
-            {
-                Console.WriteLine("Выберите режим игры");
-                Console.WriteLine("1. Автоматическая игра (первоначальная позиция <живых> клеток расставляется рандомно)");
-                Console.WriteLine("2. Классическая игра (пользователь сам расставляет <живые> клетки)");
-                int a = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Введите размерность поля N*M, каждое число с новой строки");
-                l.N = Convert.ToInt32(Console.ReadLine());
-                l.M = Convert.ToInt32(Console.ReadLine());
-                return a;
-            }
             //поле
             Life lifeObj = new Life();
 
@@ -75,21 +95,35 @@ namespace LifeGame
             {
                 //автоматическая игра
                 case 1:
+                    //заполняем поле рандомными клетками
                     lifeObj.RandomSetArr();
+                    //выводим начальную позицию
                     Console.WriteLine("Поле:");
-                    lifeObj.ReadArr();
-                    Step(lifeObj);//запусткаем цикл проверок
+                    ReadArr(lifeObj);
+                    //запускаем цикл проверок
+                    do
+                    {
+                        lifeObj.Step();
+                        //выводим результат шага
+                        Console.WriteLine();
+                        ReadArr(lifeObj);
+                    } while (!AreAllDead(lifeObj));
                     break;
                 //классическая игра
                 case 2:
-                    Console.WriteLine("Введите число, которое будет инициализировать клетку <живой> или <мертвой>");
-                    Console.WriteLine("Каждое значение с новой строки");
-                    Console.WriteLine("1. Клетка <живая>");
-                    Console.WriteLine("2. Клетка <мертвая>");
-                    lifeObj.SetArr();
+                    //заполняем поле
+                    SetArr(ref lifeObj);
+                    //выводим начальную позицию
                     Console.WriteLine("Поле:");
-                    lifeObj.ReadArr();
-                    Step(lifeObj);//запускаем цикл проверок
+                    ReadArr(lifeObj);
+                    //запускаем цикл проверок
+                    do
+                    {
+                        lifeObj.Step();
+                        //выводим результат шага
+                        Console.WriteLine();
+                        ReadArr(lifeObj);
+                    } while (!AreAllDead(lifeObj));
                     break;
             }
 
