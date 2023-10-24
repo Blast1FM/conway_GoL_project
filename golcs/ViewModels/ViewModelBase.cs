74a5a08b2752c41e9095bbf8c97b256cd8526cb0
Blast1FM <kingprostyle@yandex.ru>
2023-10-24 18:18:57 +0300

    return to mvvm

---
+++ golcs/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace golcs.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
--- golcs/Views/MainWindow.axaml.cs
+++ golcs/Views/MainWindow.axaml.cs
@@ -1,6 +1,6 @@
 using Avalonia.Controls;
 
-namespace golcs;
+namespace golcs.Views;
 
 public partial class MainWindow : Window
 {
