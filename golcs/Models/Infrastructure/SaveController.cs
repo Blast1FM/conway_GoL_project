62e541ccef7d042aca4437e90abc39f989905d70
Blast1FM <kingprostyle@yandex.ru>
2023-10-30 17:44:50 +0300

    Save controller placeholders

--- golcs/Models/Infrastructure/SaveController.cs
+++ golcs/Models/Infrastructure/SaveController.cs
@@ -1,5 +1,33 @@
+using System.Collections.Generic;
+using golcs.Models.World;
+
 namespace golcs.Models.Infrastructure;
 public class SaveController
 {
+    public List<GameState>? save_list;
 
+    //placeholder - need to figure out pathing and serialisation
+    public bool Load_all_saves()
+    {
+        return true;
+    }
+
+    //placeholder
+    public GameState Return_game_state_by_username(string player_name)
+    {
+        return null;
+    }
+
+    //Helper method for Save_game
+    public bool Overwrite_save(GameState game_state)
+    {
+        return true;
+    }
+
+    //placeholder - Updates the save list if gamestate with its username exists, adds a new entry otherwise
+    //true if successful
+    public bool Save_game(GameState game_state)
+    {
+        return true;
+    }
 }
