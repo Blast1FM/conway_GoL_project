0ec99951236c4ace1d13a2601393474ee01b1ff7
Blast <kingprostyle@yandex.ru>
2023-11-05 15:20:30 +0300

    help

---
+++ golcs/Models/World/ObservableGameState.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace golcs.Models.World;
+
+public class ObservableGameState : ObservableObject   
+{
+    private readonly GameState gamestate;
+
+    public ObservableGameState(GameState gamestate) => this.gamestate = gamestate;
+
+    public int High_score
+    {
+        get => gamestate.high_score;
+        //I don't get this shit, send help
+        set => SetProperty(gamestate.high_score, value, gamestate, (state, score) => state.high_score = score);
+    }
+}
---
+++ golcs/Views/CellGridView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace golcs.Views;
+
+public partial class CellGridView : UserControl
+{
+    public CellGridView()
+    {
+        InitializeComponent();
+    }
+}
