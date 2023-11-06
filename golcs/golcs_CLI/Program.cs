f1f88369afd0388b73db8fb1f5aa5639e8f27d18
Blast <kingprostyle@yandex.ru>
2023-11-06 17:06:28 +0300

    menus and stuff

--- golcs/golcs_CLI/Program.cs
+++ golcs/golcs_CLI/Program.cs
@@ -5,6 +5,22 @@ namespace golcs_CLI;
 
 class Program
 {
+    static int Get_int_from_console()
+    {
+        if(int.TryParse(Console.ReadLine(), out int number)) return number;
+        else return -1; 
+    }
+    static int Main_menu(int commandID)
+    {
+        System.Console.WriteLine
+        (
+            @"1.Start new game
+            2.Load megasave
+            3.View scoreboard
+            0.Exit"
+        );
+        return Get_int_from_console();
+    }
     static void Main(string[] args)
     {
         GameState gameState = new("Blast", 16);
