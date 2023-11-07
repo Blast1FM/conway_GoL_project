409229a3d69d2837f73fa027029dcfbb5db0f7a2
Blast <kingprostyle@yandex.ru>
2023-11-07 17:27:09 +0300

    note to self

--- golcs/golcs_CLI/UI/Menus.cs
+++ golcs/golcs_CLI/UI/Menus.cs
@@ -2,17 +2,31 @@
 
 public class Menus
 {
+    //#TODO implement string input, for filenames
     public static int Get_int_from_console()
     {
         if(int.TryParse(Console.ReadLine(), out int number)) return number;
         else return -1; 
     }
-    public static int Main_menu()
+    public static int Init_menu()
     {
         System.Console.WriteLine
         (
             @"1.Load megasave
             2.Create new megasave
+            0.Exit"
+        );
+        return Get_int_from_console();
+    }
+
+
+    public static int Main_menu()
+    {
+        System.Console.WriteLine
+        (
+            @"1.Load a saved game
+            2.Start new game
+            3.View scoreboard
             0.Exit"
         );
         return Get_int_from_console();
