c375b65a82c94ecddf47fdee560b4582479b6e4a
Blast <kingprostyle@yandex.ru>
2023-11-07 16:46:57 +0300

    WIP, not finished yet

--- golcs/golcs_CLI/Renderer/Renderer.cs
+++ golcs/golcs_CLI/Renderer/Renderer.cs
@@ -4,9 +4,8 @@ namespace golcs.Renderer;
 
 public static class Renderer
 {
-    public static void Display_grid(ref Grid grid)
+    private static void Display_grid(ref Grid grid)
     {
-        System.Console.WriteLine();
         foreach (var row in grid.cell_list)
         {
             foreach (var item in row)
@@ -17,8 +16,20 @@ public static class Renderer
         }
     }
 
-    public static void Display_bottom_bar(string bar)
+    private static void Display_bottom_bar(string bar)
     {
         System.Console.WriteLine(bar);
+    }
+
+    private static void Display_game_stats(ref GameState gameState)
+    {
+        System.Console.WriteLine($"Player name: {gameState.player_name} Generation:{gameState.generation_count} High score:{gameState.high_score} Cells alive:{gameState.current_live_cells}");
+    }
+
+    public static void Display_game(ref GameState gameState)
+    {
+        Display_game_stats(ref gameState);
+        Display_grid(ref gameState.current_grid);
+        Display_bottom_bar("placeholder");
     }
 }
---
+++ golcs/golcs_CLI/UI/Menus.cs
@@ -0,0 +1,20 @@
+﻿namespace golcs.UI;
+
+public class Menus
+{
+    public static int Get_int_from_console()
+    {
+        if(int.TryParse(Console.ReadLine(), out int number)) return number;
+        else return -1; 
+    }
+    public static int Main_menu()
+    {
+        System.Console.WriteLine
+        (
+            @"1.Load megasave
+            2.Create new megasave
+            0.Exit"
+        );
+        return Get_int_from_console();
+    }
+}
